c7043abdba12ef6b93a35abdb5957780dfef74cb
Daniel de Jesus Araujo <danielaraljo115@gmail.com>
2022-05-06 15:35:30 -0300

    formata

--- modulo_01:fundamentos/bloco-3-introducao-a-html-e-css/dia-1-html-css-estruturas-de-pagina/script.js
+++
@@ -1,8 +0,0 @@
-function calcular(){
-    let num = document.getElementById('num')
-    let resp = document.getElementById('res')
-
-    let soma = num.value * 2
-    resp.innerHTML = 'aqui vai o resultado,'
-    resp.innerHTML += soma
-}
